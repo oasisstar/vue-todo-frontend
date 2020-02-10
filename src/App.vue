2d3c11d86e666d1ee38c1e2bfb5e3a7fc849ebba
oasisstar <mrtOasis117@hotmail.com>
2020-02-10 16:30:00 +0700

    vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+// import Login from './components/Auth/Login'
+// import Signup from './components/Auth/Signup'
+import Todo from './components/Todo'
+
+export default {
+  name: 'app',
+  components: {
+    Todo
+  }
+}
+</script>
